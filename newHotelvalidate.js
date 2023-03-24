b6feb1473bfae8fc5ce00af3d05bda6bcf9effb6
Debarghya Mondal <debarghyamondal15@gmail.com>
2023-03-24 15:26:32 +0530

    minor change

--- newHotelvalidate.js
+++ newHotelvalidate.js
@@ -33,11 +33,11 @@ function validation() {
   //   return false;
   // }
   const pass = document.getElementById("passwordfirst").value;
-  const pattern = /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{8}/;
+  const pattern = /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{8,}/;
   if (!pass.match(pattern)) {
     const passError = document.getElementById("nameerror");
     passError.innerHTML =
-      "<ul> <li>at least one uppercase letter </li><li>at least one lowercase letter</li> <li> at least one digit </li><li>at least one special character</li><li> Password length: 8 characters </li></ul>.";
+      "<ul> <li>at least one uppercase letter </li><li>at least one lowercase letter</li> <li> at least one digit </li><li>at least one special character</li><li> Password length: 8 characters minimum </li></ul>.";
     return false;
   }
   const confpass = document.getElementById("password").value;
