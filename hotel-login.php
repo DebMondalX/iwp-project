cb12f42a3a67af7689d1c544c2a73da268f29797
Debarghya Mondal <debarghyamondal15@gmail.com>
2023-04-05 01:25:20 +0530

    new func

---
+++ hotel-login.php
@@ -0,0 +1,32 @@
+<?php
+    $name=$pass="";
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['username'];
+        $pass = $_POST['pswd'];
+    }
+    //echo $name;
+    //echo $password;
+    include "connection.php";
+    //$sql = "select * from admin_cred where username = '$name' and password ='$pass'"
+    $sql = "select * from hotel_register where email ='$name' and password = '$pass'";
+    $result = mysqli_query($conn, $sql);
+    
+   // mysqli_store_result($conn);
+
+       // echo "Total No. of records :".mysqli_num_rows($result);
+    
+        
+    
+    if( mysqli_num_rows($result) > 0)
+    {
+       header("Location: hotel-homepage.php");
+    }
+    else
+    {
+        echo "Login unsuccessful";
+    }
+    mysqli_close($conn);
+    
+    
+?>
--- viewhotels.php
+++ viewhotels.php
@@ -5,10 +5,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <style>
+      h3 {
+        text-align:center;
+        background: aqua;
+      }
+    </style>
   </head>
   <body>
     <?php
-      echo "<h3> The Hotels Registered With SuitCase :</h3>"
+      echo "<h3> The Hotels Registered With SuitCase :</h3>";
+
+        function openConnection(){
+            $username = "root";
+            $password = "";
+            $servername = "127.0.0.1";
+            $dbname = "iwp";
+
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            if($conn)
+            {
+                return $conn;
+            }
+            else
+            {
+                echo ("Connection to DB failed: ".mysqli_connect_error());
+            }
+            }
+            function closeConnection($conn){
+                $conn->close();
+            }
+
+            $conn = openConnection();
+            
+            $sql = "select * from hotel_register;";
+
+
+    $result = mysqli_query($conn,$sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table border = 1>
+              <tr>
+              <th>Hotel Name</th>
+              <th>email ID</th>
+              <th>Hotel Type</th>
+              <th>Phone</th>
+              <th>Destination</th>
+              </tr>
+              ";
+        while($row = mysqli_fetch_array($result)){
+              echo "<tr>";
+              echo "<td>".$row['hotel_name']."</tid>";
+              echo "<td>".$row['email']."</tid>";
+              echo "<td>".$row['hotel_type']."</tid>";
+              echo "<td>".$row['phone_no']."</tid>";
+              echo "<td>".$row['destination']."</tid>";
+              echo "</tr>";
+        }
+            echo "</table>";
+        }
+        else{
+            echo "No Hotels have registered.";
+        }
+        closeConnection($conn);
 
       
     ?>
